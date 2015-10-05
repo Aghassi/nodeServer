0e98869303df512ec6a34b23e1f0bb0c9332229c
David Aghassi <david.aghassi@gmail.com>
2015-10-05 19:15:46 -0400

    Used express to create a basic express server

--- hello.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {
-   res.writeHead(200, {'Content-Type': 'text/plain'});
-   res.end('Hello World\n');
-}).listen(9000, 'localhost');
-console.log('Server is running on localhost:9090');
